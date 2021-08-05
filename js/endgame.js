eba59d6969752a608ff927eda88a37739636a54a
Cameron Houck <cameronhouck1@gmail.com>
2021-08-04 21:54:05 -0400

    adding most recent score

--- js/endgame.js
+++ js/endgame.js
@@ -6,3 +6,5 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 const highestScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 3;
+
+finalScore.innerText = mostRecentScore;
